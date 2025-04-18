61b1291d6693e2f715ed3b3da427cfdddb8d849f
PHM-dung2 <pp4992@naver.com>
2025-04-18 10:22:42 +0900

    4/18 박희만 : mvc 파일 생성 / 백엔드

---
+++ src/main/java/devconnect/model/dto/DeveloperDto.java
@@ -0,0 +1,16 @@
+package devconnect.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data @NoArgsConstructor @AllArgsConstructor
+public class DeveloperDto {
+    private int dno;
+    private String did;
+    private String dpwd;
+    private String dname;
+    private
+}
