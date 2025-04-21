ebc203d3906c64bd8541547220f2857c779b3401
leeminjin0827 <lmj98202@gmail.com>
2025-04-21 18:15:50 +0900

    4/21 이민진 : 개발자 , 기업 개별조회 생성 , DTO 수정 , 개발자 평가 등록 , 전체조회 완료

--- src/main/java/devconnect/model/dto/CratingDto.java
+++ src/main/java/devconnect/model/dto/CratingDto.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,9 +15,11 @@ import lombok.NoArgsConstructor;
 public class CratingDto {
     private int crno; // 평가 번호(기업)
     private int crscore; // 점수(기업)
-    private String crdate; // 평가일(기업)
+    private LocalDateTime crdate; // 평가일(기업)
     private int dno; // 개발자번호(FK)
     private int pno; // 프로젝트번호(FK)
+    private LocalDateTime createAt; // 등록날짜
+    private LocalDateTime updateAt; // 수정날짜
     
     // Dto -> Entity
     public CratingEntity toEntity(){
--- src/main/java/devconnect/service/DratingService.java
+++ src/main/java/devconnect/service/DratingService.java
@@ -1,11 +1,14 @@
 package devconnect.service;
 
 import devconnect.model.dto.DratingDto;
+import devconnect.model.entity.DratingEntity;
 import devconnect.model.repository.DratingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +19,33 @@ public class DratingService {
     // 개발자 평가 등록
     public boolean dratingWrite(DratingDto dratingDto ){
         System.out.println("DratingService.dratingWrite");
-        return true;
+        DratingEntity dratingEntity = dratingDto.toEntity();
+        DratingEntity saveEntity = dratingRepository.save( dratingEntity );
+        if( saveEntity.getDrno() >= 1 ){ return true; }
+        return false;
     } // f end
     
     // 개발자 평가 전체 조회
     public List<DratingDto> dratingList(){
         System.out.println("DratingService.dratingList");
+        List<DratingEntity> dratingListAll = dratingRepository.findAll();
+        List<DratingDto> dratingList = new ArrayList<>();
+        for( int i = 0; i <= dratingListAll.size() -1; i++ ){
+            DratingDto dratingDto = dratingListAll.get(i).toDto();
+            dratingList.add( dratingDto );
+        } // for end
+        return dratingList;
+    } // f end
+
+    // 개발자 평가 개별 조회
+    public DratingDto dratingView( int drno ){
+        System.out.println("DratingService.dratingView");
+        Optional<DratingEntity> dOptional = dratingRepository.findById( drno );
+        if ( dOptional.isPresent() ){
+            DratingEntity dratingEntity = dOptional.get();
+            DratingDto dratingDto = dratingEntity.toDto();
+            return dratingDto;
+        } // if end
         return null;
     } // f end
     
