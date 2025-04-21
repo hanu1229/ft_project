dc797447ff1dd49fcc3ae4150128a28035423c9a
hanu1229 <han494127@gmail.com>
2025-04-21 18:27:47 +0900

    4/21 취합 : 필요없는 클래스 파일 삭제 및 오류 주석 처리

--- src/main/java/devconnect/model/repository/DratingRepository.java
+++ src/main/java/devconnect/model/repository/DratingRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DratingRepository extends JpaRepository<DratingEntity, Integer> {
 
-    DratingEntity findByPno
+    // DratingEntity findByPno
 } // interface end
