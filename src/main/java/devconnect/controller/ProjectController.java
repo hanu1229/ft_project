7498a0e9c6a67ce1bd626f1123da1e6531bf6888
hanu1229 <han494127@gmail.com>
2025-04-21 18:14:05 +0900

    4/21 한웅재 : 프로젝트, 프로젝트 신청 테이블의 샘플 데이터 추가, 프로젝트 등록 및 조회 구현 중

--- src/main/java/devconnect/controller/ProjectController.java
+++ src/main/java/devconnect/controller/ProjectController.java
@@ -1,10 +1,16 @@
 package devconnect.controller;
 
+import devconnect.model.dto.ProjectDto;
 import devconnect.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -13,5 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController {
 
     private final ProjectService projectService;
+    
+    /// ● 프로젝트 등록
+    @PostMapping("")
+    public boolean writeProject(@RequestBody() ProjectDto projectDto) {
+        System.out.println("ProjectController.writeProject");
+        System.out.println("projectDto = " + projectDto);
+        return projectService.writeProject(projectDto);
+    }
+    
+    /// ● 프로젝트 조회
+    @GetMapping("")
+    public List<ProjectDto> findAllProject() {
+        System.out.println("ProjectController.findAllProject");
+        return projectService.findAllProject();
+    }
+
+    /// ● 프로젝트 수정
+
+
+    /// ● 프로젝트 삭제
+    
 
 }
