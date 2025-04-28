330881a86202a1b05d49e741524c06e6d0b350e2
hanu1229 <han494127@gmail.com>
2025-04-28 18:35:38 +0900

    4/28 취합 : DeveloperController.java에 @CrossOrigin("*") 적용

--- src/main/java/devconnect/controller/DeveloperController.java
+++ src/main/java/devconnect/controller/DeveloperController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/developer")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class DeveloperController {
     private final DeveloperService developerService;
 
