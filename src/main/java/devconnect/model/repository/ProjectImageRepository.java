7039d19bc6be7d759fdeb1f5f759fc5d032b838e
hanu1229 <han494127@gmail.com>
2025-05-09 18:17:30 +0900

    05/09 배포 1. 파일 유틸 수정

--- src/main/java/devconnect/model/repository/ProjectImageRepository.java
+++ src/main/java/devconnect/model/repository/ProjectImageRepository.java
@@ -2,8 +2,15 @@ package devconnect.model.repository;
 
 import devconnect.model.entity.ProjectImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProjectImageRepository extends JpaRepository<ProjectImageEntity, Integer> {
+
+    @Query(value = "select * from project_image where pno = :pno", nativeQuery = true)
+    List<ProjectImageEntity> findByPno(int pno);
+
 }
